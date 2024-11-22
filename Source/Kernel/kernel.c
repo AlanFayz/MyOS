72f67b0b7bdec87f66535f4af0bd2620e411c01e
Alan Fayz <alan.fayz@hotmail.com>
2024-11-22 22:02:51 +0000

    small test

--- Source/Kernel/kernel.c
+++ Source/Kernel/kernel.c
@@ -23,29 +23,22 @@ void kernel_start()
 {
     gfx_color_t test_color;
     test_color.b = 0xFF;
-    test_color.g = 0xFF;
-    test_color.r = 0x00;
 
-    gfx_rect_2d_t rect;
-    rect.x = 800;
-    rect.y = 300;
-    rect.width = 200;
-    rect.height = 200;
+    uint32_t height = gfx_get_height();
+    uint32_t width  = gfx_get_width();
 
-    gfx_draw_rect(rect, test_color);
-    
-    gfx_circle_t circle;
-    circle.x = 300;
-    circle.y = 300;
-    circle.radius = 100;
 
-    gfx_draw_circle(circle, test_color);
+    for(uint32_t y = 0; y < height; y++)
+    {
+        for(uint32_t x = 0; x < width; x++)
+        {
+            float c = (float)y / (float)height;
+            int8_t color = (int8_t)(c * 255.0);
 
-    gfx_character_t character;
+            test_color.g = color;   
+            test_color.r = 255 - color;
 
-    character.character = 'A';
-    character.x = 50;
-    character.y = 50;
-
-    gfx_draw_character(character, test_color);
+            gfx_draw_pixel(x, y, test_color);
+        }
+    }
 }
